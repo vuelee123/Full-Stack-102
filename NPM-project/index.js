7032cff28fa144ace0111620c884d0aa400d9a56
Lee Vue <vuelee123@gmail.com>
2023-04-25 11:00:10 -0400

    NPM Activity assignment

---
+++ NPM-project/index.js
@@ -0,0 +1,10 @@
+// Require or import the 'moment' modules in your application.
+ const Moment = require('moment');
+
+// Use 'moment' to output the timestamp for when the response arrives using the format h:mm:ss a.
+ const myMoment = new Moment ();
+ console.log(myMoment.format('h:mm:ss a'));
+
+ 
+
+
