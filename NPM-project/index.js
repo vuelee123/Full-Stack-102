08d35e168887b496ecfd04f2e9b1db5c5c46b7f3
Lee Vue <vuelee123@gmail.com>
2023-04-25 20:31:03 -0400

    NPM activity- redo

--- NPM-project/index.js
+++ NPM-project/index.js
@@ -1,8 +1,18 @@
+// NPM Activity
+
+//create an npm project : npm init
+//install the 'moment'package : npm install moment
+//examine the directory
+//delete the node_modules : npm uninstall
+//install again : npm install -> both these action removed the unnecessary folders
+//updated the package.json by adding 'start' to the script.
+
+
 // Require or import the 'moment' modules in your application.
  const Moment = require('moment');
 
-// Use 'moment' to output the timestamp for when the response arrives using the format h:mm:ss a
- const myMoment = new Moment ();
+// Use 'moment' to output the time stamp for when the response arrives using the format h:mm:ss a
+ const myMoment = new Moment();
  console.log(myMoment.format('h:mm:ss a'));
 
  
