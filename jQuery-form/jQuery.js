6893e64d782134a057fa9e65fa4cc43574c3c2fc
Lee Vue <vuelee123@gmail.com>
2023-04-25 20:49:25 -0400

    jQuery form validation

--- jQuery-form/jQuery.js
+++ jQuery-form/jQuery.js
@@ -1,24 +1,24 @@
 // jQuery Form Validation assignment
 
 // create variables
-var nameId=$("#name");
-var emailId=$('#email');
-var phoneId=$('#phone');
-var message=$('#message')
+var nameId= $("#name");
+var emailId= $('#email');
+var phoneId= $('#phone');
+
 
 // create an array
-var required = [nameId,emailId,phoneId];
+var required = [nameId, emailId, phoneId];
 
 // create a function
 $(document).ready(function() {
     $('#submit').on('click', function(){
-    for (var i = 0; i < required.length; i++){ // loop for entire required array
-        if (required[i].val () == ""){
-        message.html('Please Fill Out the Required Fields')
+    for (var i = 0; i < required.length; i++) { // create a for loop
+        if (required[i].val () === "") {
+        $('#message').html('Please Fill Out the Required Fields');
         required[i].prev().addClass('warning');
-        message.addClass('warning')
+        $('#message').addClass('warning');
         }
-        if (required[i].val () !== ""){
+        if (required[i].val () !== "") {
         required[i].prev().removeClass('warning');
         }
         if (!$("label").hasClass("warning")) { // another if statement
